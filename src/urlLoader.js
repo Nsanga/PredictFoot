5107bda0fbee6a05fe5dd879e53bb769d65c5ecb
Mercure Mekinda <mercuremekinda@gmail.com>
2023-06-20 13:12:43 +0100

    get all article on screen blog

--- src/urlLoader.js
+++ src/urlLoader.js
@@ -1,6 +1,6 @@
 const localURL = "http://localhost:5000";
-const liveURL = "";
-console.log("::::",localURL);
+const liveURL = "http://45.92.111.13:5000";
+console.log("::::",liveURL);
 
 module.exports = {
     url:process.env.NODE_ENV ===  'production' ? liveURL : localURL
